9cdedf7c30b0df53935df64266d515ee8b1877f5
NaneMartirosyan <nanemartirosyan.tt219-2@polytechnic.am>
2024-11-30 21:10:29 +0400

    Update vehicle.py

--- lec_10/vehicle.py
+++ lec_10/vehicle.py
@@ -1,1 +1,8 @@
+class Vehicle:
+    def __init__(self, name, speed):
+        self.name = name
+        self.speed = speed
+
+    def move(self):
+        return f"The {self.name} moves at a speed of {self.speed} km/h."
 
