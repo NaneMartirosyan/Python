822032f7f5db8d7c63e7dcee071f5bb9638bff18
NaneMartirosyan <nanemartirosyan.tt219-2@polytechnic.am>
2024-11-30 14:08:44 +0400

    Update main.py

--- lec_4/main.py
+++ lec_4/main.py
@@ -1,1 +1,18 @@
+numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+print("List of numbers:", numbers)
+even = []
+odd = []
 
+def classify_numbers(number):
+    for num in number:
+        if num % 2 == 0:
+            even.append(num)
+        elif num % 2== 1:
+            odd.append(num)
+
+    return even, odd
+
+zuyg, kent =classify_numbers(numbers)
+
+print(zuyg)
+print(kent)
